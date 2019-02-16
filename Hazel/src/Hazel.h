a97af716fa89481b803dced314544427b45c2142
AJAY SINGH GODARA <71godara@gmail.com>
2019-02-16 13:21:22 +0530

    Basic Setup and Entry point addition

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// ONLY for use by the application and not the Hazel engine itself 
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// --------------------------------
+// ----Entry Point-----------------
+// --------------------------------
+#include "Hazel/EntryPoint.h"
+
