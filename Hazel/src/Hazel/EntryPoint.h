62a1b94ac46c61f259e88a66c9a942c232cfc5d7
AJAY SINGH GODARA <71godara@gmail.com>
2019-02-16 23:52:53 +0530

    Added Premake, TODO:Fix broken changes due to spdlog update

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -8,11 +8,10 @@ int main(int argc, char** argv)
 {
 	printf("Hazel Engine!\n");
 	Hazel::Log::Init();
-	HZ_CORE_WARN("Initialized Log! ");
-	HZ_WARN("Initialized Log! ");
-	int a{ 5 };
-	HZ_INFO("The variable value is {0}", a);
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_WARN("Initialized Log!");
 	
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
