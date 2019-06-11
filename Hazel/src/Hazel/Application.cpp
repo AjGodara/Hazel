1ecdf36628f347c44f6d55e4d36926ebae7f9b3e
AJAY SINGH GODARA <71godara@gmail.com>
2019-06-11 23:22:57 +0530

    ...

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -49,7 +49,7 @@ namespace Hazel {
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
-		HZ_CORE_INFO("{0}", e);
+		//HZ_CORE_INFO("{0}", e);
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();)
 		{
