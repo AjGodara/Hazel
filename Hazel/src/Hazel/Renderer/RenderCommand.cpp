a8f318a27a64755fda851b8c1c2ea9c9170a8138
AJAY SINGH GODARA <71godara@gmail.com>
2019-09-12 19:20:46 +0530

    Added VertexArray abstraction and Renderer flow classes: Renderer API, RenderCommand and OpenGLRendererAPI

---
+++ Hazel/src/Hazel/Renderer/RenderCommand.cpp
@@ -0,0 +1,10 @@
+#include "hzpch.h"
+
+#include "RenderCommand.h"
+
+#include "platform/OpenGL/OpenGLRendererAPI.h"
+
+namespace Hazel {
+
+	RendererAPI* RenderCommand::s_RendererAPI = new OpenGLRendererAPI;
+}
